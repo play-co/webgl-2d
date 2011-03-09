e09ef5099cbbd3f0e32b8555c398001c5d6c6e24
David Humphrey <david.humphrey@senecac.on.ca>
2011-03-08 23:54:15 -0500

    Deal with canvas width=height=0 and webgl failure.

--- webgl-2d.js
+++ webgl-2d.js
@@ -245,7 +245,8 @@
     // Override getContext function with "webgl-2d" enabled version
     canvas.getContext = (function(gl2d) { 
       return function(context) {
-        if (gl2d.options.force || context === "webgl-2d") {
+        if (gl2d.options.force || context === "webgl-2d" &&
+            !(canvas.width === 0 || canvas.height ===0) ) {
           if (gl2d.gl) { return gl2d.gl; };
 
           var gl = gl2d.gl = gl2d.canvas.$getContext("experimental-webgl");
